313b035f60ec7ed0ce25f5f0a29a3d9fa0ccc516
yeelalor <yeelalorchoulor@gmail.com>
2023-07-21 21:03:32 +0700

    new

--- lib/screens/Splash/splash_screen.dart
+++ lib/screens/Splash/splash_screen.dart
@@ -20,49 +20,55 @@ class _SplashPageState extends State<SplashPage> {
   void initState() {
     super.initState();
     Timer(
-        const Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => const Login())));
+      const Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const Login(),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          alignment: Alignment.center,
-          height: MediaQuery.of(context).size.height,
+        alignment: Alignment.center,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Container(
           width: MediaQuery.of(context).size.width,
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(24.0),
-                  child: Image.asset(
-                    "assets/logos/logo w ldb.png",
-                    height: 100.0,
-                    width: 100.0,
-                  ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(24.0),
+                child: Image.asset(
+                  "assets/logos/logo w ldb.png",
+                  height: 100.0,
+                  width: 100.0,
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  "LDB Support",
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  child: spinkit,
-                ),
-              ],
-            ),
-          )),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "LDB Support",
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width,
+                child: spinkit,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
